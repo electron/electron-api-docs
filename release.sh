4a2de3498c4b6014237b4a555f102eb9c4b06da0
Zeke Sikelianos <zeke@sikelianos.com>
2016-09-23 12:28:40 -0700

    update release script

--- release.sh
+++ release.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
+# This release script is intended to be run on Heroku, not locally.
+
 set -x            # print each command before execution
 set -o errexit    # always exit on error
 set -o pipefail   # don't ignore exit codes when piping output
 set -o nounset    # fail on unset variables
 
 # set up the repo
+git clone https://github.com/electron/electron-api-docs
+cd electron-api-docs
+npm install
 npm run build
 npm test
 
@@ -21,3 +26,7 @@ git commit -m "API docs for Electron $version"
 npm version $version -m "API docs for Electron $version"
 npm publish
 git push origin master --follow-tags
+
+# clean up
+cd ..
+rm -rf electron-api-docs
